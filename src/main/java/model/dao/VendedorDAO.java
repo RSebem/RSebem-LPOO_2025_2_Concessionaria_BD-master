0630d17b64775531a0e0bc95a31a68040ef8d745
RSebem <renahnsebem.pf025@academico.ifsul.edu.br>
2025-11-05 19:11:40 -0300

    Add files via upload

---
+++ src/main/java/model/dao/VendedorDAO.java
@@ -0,0 +1,27 @@
+package model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import model.Cliente;
+import model.Vendedor;
+
+/**
+ *
+ * @author vanessalagomachado
+ */
+public class VendedorDAO extends PersistenciaJPA{
+    
+    public List<Vendedor> listaVendedores(){
+    EntityManager em = getEntityManager();
+        try {
+            TypedQuery<Vendedor> query
+                    = em.createQuery("SELECT v FROM Vendedor v", Vendedor.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
